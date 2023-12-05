382b7d6b61fa1abea91a26b47f43094796013bd2
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-12-05 23:34:48 +0100

    placePiece method

--- ticTacToe.java
+++ ticTacToe.java
@@ -12,6 +12,14 @@ public class ticTacToe {
 
         printGameBoard(gameBoard);
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nEnter your placement (1-9):");
+        int pos = scan.nextInt();
+
+        placePiece(gameBoard, pos, "player");
+
+        printGameBoard(gameBoard);
+
     }
 
     public static void printGameBoard(char[][] gameBoard) {
@@ -20,6 +28,50 @@ public class ticTacToe {
                 System.out.print(c);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int pos, String user) {
+
+        char symbol = ' ';
+
+        if (user.equals("player")) {
+            symbol = 'X';
+        } else if (user.equals("cpu")) {
+            symbol = 'O';
+        }
+
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
+
         }
     }
 }
