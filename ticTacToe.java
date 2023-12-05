4c9460cea06451e0b57f399d08e0305eab1373d7
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-12-05 23:23:05 +0100

    printGameBoard method

--- ticTacToe.java
+++ ticTacToe.java
@@ -10,6 +10,11 @@ public class ticTacToe {
                 { '-', '+', '-', '+', '-' },
                 { ' ', '|', ' ', '|', ' ' }, };
 
+        printGameBoard(gameBoard);
+
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
         for (char[] row : gameBoard) {
             for (char c : row) {
                 System.out.print(c);
