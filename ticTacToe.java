b74c344ff3f4d745a6938ac5294cb1731eec8e63
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-12-05 23:21:33 +0100

    gameboard

--- ticTacToe.java
+++ ticTacToe.java
@@ -4,5 +4,17 @@ import java.io.*;
 public class ticTacToe {
     public static void main(String[] args) {
 
+        char[][] gameBoard = { { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' }, };
+
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
